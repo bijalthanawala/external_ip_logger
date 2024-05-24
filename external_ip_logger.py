8d73a347da49ddfce3029aff0c13054cc7b4f493
Bijal Thanawala <bijalthanawala@users.noreply.github.com>
2024-05-25 01:22:07 +1000

    Type hints added

--- external_ip_logger.py
+++ external_ip_logger.py
@@ -4,23 +4,27 @@ import sys
 import time
 import datetime
 import urllib.request
+from http.client import HTTPResponse
 
-prev_ip_addr = ''
-start_time = None
-end_time = None
+prev_ip_addr: str = ""
+start_time: str = ""
+end_time: str = ""
+
 while True:
-    now = datetime.datetime.now()
-    response = urllib.request.urlopen("https://ifconfig.me")
-    content = response.read()
+    now: datetime.datetime = datetime.datetime.now()
+    response: HTTPResponse = urllib.request.urlopen("https://ifconfig.me")
+    content: bytes = response.read()
     response.close()
-    ip_addr = content.decode()
-    curr_time = time.strftime("%Y%m%d_%H%M%S")
-    print(f"Current time: {curr_time}\tCurrent IP: {ip_addr}", file=sys.stderr, end="\r")
+    ip_addr: str = content.decode()
+    curr_time: str = time.strftime("%Y%m%d_%H%M%S")
+    print(
+        f"Current time: {curr_time}\tCurrent IP: {ip_addr}", file=sys.stderr, end="\r"
+    )
     if not prev_ip_addr:
         start_time = curr_time
         prev_ip_addr = ip_addr
     elif prev_ip_addr != ip_addr:
-        print(f"", file=sys.stderr)
+        print("", file=sys.stderr)
         print(f"{start_time},{curr_time},{prev_ip_addr}")
         start_time = curr_time
         prev_ip_addr = ip_addr
