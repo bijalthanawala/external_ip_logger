8f43bffb56049b6fe65ebaacf0c9dde2fada29ad
Bijal Thanawala <bijalthanawala@users.noreply.github.com>
2024-05-25 01:37:23 +1000

    Placed the script logic in the method main()

--- external_ip_logger.py
+++ external_ip_logger.py
@@ -2,32 +2,37 @@
 
 import sys
 import time
-import datetime
 import urllib.request
 from http.client import HTTPResponse
 
-prev_ip_addr: str = ""
-start_time: str = ""
-end_time: str = ""
 
-print("start_time,end_time,iP_address", sys.stdout)
-while True:
-    now: datetime.datetime = datetime.datetime.now()
-    response: HTTPResponse = urllib.request.urlopen("https://ifconfig.me")
-    content: bytes = response.read()
-    response.close()
-    ip_addr: str = content.decode()
-    curr_time: str = time.strftime("%Y%m%d_%H%M%S")
-    print(
-        f"Current time: {curr_time}\tCurrent IP: {ip_addr}", file=sys.stderr, end="\r"
-    )
-    if not prev_ip_addr:
-        start_time = curr_time
-        prev_ip_addr = ip_addr
-    elif prev_ip_addr != ip_addr:
-        print("", file=sys.stderr)
-        print(f"{start_time},{curr_time},{prev_ip_addr}")
-        start_time = curr_time
-        prev_ip_addr = ip_addr
+def main() -> None:
+    prev_ip_addr: str = ""
+    start_time: str = ""
 
-    time.sleep(5)
+    print("start_time,end_time,ip_address", sys.stdout)
+    while True:
+        response: HTTPResponse = urllib.request.urlopen("https://ifconfig.me")
+        content: bytes = response.read()
+        response.close()
+        ip_addr: str = content.decode()
+        curr_time: str = time.strftime("%Y%m%d_%H%M%S")
+        print(
+            f"Current time: {curr_time}\tCurrent IP: {ip_addr}",
+            file=sys.stderr,
+            end="\r",
+        )
+        if not prev_ip_addr:
+            start_time = curr_time
+            prev_ip_addr = ip_addr
+        elif prev_ip_addr != ip_addr:
+            print("", file=sys.stderr)
+            print(f"{start_time},{curr_time},{prev_ip_addr}", file=sys.stdout)
+            start_time = curr_time
+            prev_ip_addr = ip_addr
+
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
